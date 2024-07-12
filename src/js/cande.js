80195372de904332a367f979faae119efa201129
candela.b.r <candela.belenrodriguez23@gmail.com>
2024-07-11 22:14:41 -0300

    cambios catalogo y mis diseños

---
+++ src/js/cande.js
@@ -0,0 +1,11 @@
+console.log("HOLA MUNDO");
+console.log(2 + 2);
+
+var s = "Hola, me llamo Juan"; // s, de string
+ var n = 28; // n, de número
+ var b = true; // b, de booleano
+ var u; // u, de undefined
+ console.log(typeof s);
+ console.log(typeof n);
+ console.log(typeof b);
+ console.log(typeof u);
